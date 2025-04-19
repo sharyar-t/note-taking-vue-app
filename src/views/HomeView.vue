0d1afbb917639afb0734425e1a40bfbd26d6e199
sharyar-dev <onshariiit@gmail.com>
2025-04-19 11:52:51 +0500

    Add Nuxt UI integration and update project configuration

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts"></script>
 
 <template>
-  <main>
-    <h1>Home</h1>
+  <main class="container mx-auto px-4 py-2">
+    <h1 class="text-center text-2xl font-bold">Home</h1>
   </main>
 </template>
